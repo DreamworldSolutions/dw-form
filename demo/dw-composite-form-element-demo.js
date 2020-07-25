90331c625d485a74e347d71be44b4aa83a1ec19d
shakil_mansuri_dream <shakil2796>
2020-07-25 16:41:26 +0530

    feat: use our lit element instead of lit-element base class.

--- demo/dw-composite-form-element-demo.js
+++ demo/dw-composite-form-element-demo.js
@@ -8,7 +8,8 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-import { css, LitElement, html } from 'lit-element';
+import { css, html } from 'lit-element';
+import { LitElement } from '@dreamworld/pwa-helpers/lit-element.js';
 import '../dw-composite-form-element'; 
 import '@dreamworld/dw-input';
 import { flexLayout } from '@dreamworld/flex-layout/flex-layout';
